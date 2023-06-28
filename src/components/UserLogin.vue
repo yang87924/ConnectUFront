0baaaba9b6e5e20f248f9fe86f0f018d12a0c1d2
Snoop33 <gpx5992189@gmail.com>
2023-06-29 01:24:05 +0800

    New629

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -6,9 +6,9 @@
       <v-toolbar-title> ConnectU </v-toolbar-title>
       <v-spacer></v-spacer>
       <!-- 登录按钮，点击跳转到 "/login" 路由 -->
-      <v-btn text to="/UserLogin"> Login </v-btn>
+      <v-btn text to="/UserLogin"> 登入 </v-btn>
       <!-- 注册按钮，点击跳转到 "/register" 路由 -->
-      <v-btn text to="/UserRegister"> Register </v-btn>
+      <v-btn text to="/UserRegister"> 創建 </v-btn>
     </v-app-bar>
     <!-- 主要內容 -->
     <v-main class="login-container">
@@ -21,76 +21,51 @@
             <h2>登入</h2>
             <!-- 登入標題 -->
             <h3>歡迎回來！</h3>
-            <!-- 歡迎訊息 -->
-            <!-- 登入表單 -->
             <v-form>
               <!-- 電子郵件輸入框 -->
-              <v-text-field
-                v-model="email"
-                label="Email"
-                outlined
-              ></v-text-field>
+              <v-text-field v-model="email" label="Email" outlined></v-text-field>
               <!-- 密碼輸入框 -->
-              <v-text-field
-                v-model="password"
-                label="Password"
-                type="password"
-                outlined
-              ></v-text-field>
+              <v-text-field v-model="password" label="Password" type="password" outlined></v-text-field>
               <a href="">忘記密碼？</a>
               <!-- 忘記密碼連結 -->
               <!-- 錯誤訊息提示 -->
-              <v-alert
-                type="error"
-                color="red"
-                icon="mdi-alert-circle"
-                class="custom-alert"
-                v-if="loginResponse"
-                dense
-                text
-              >
+              <v-alert :type="loginAlertType" :color="loginAlertType === 'error' ? 'red' : 'green'"
+                :icon="loginAlertType === 'error' ? 'mdi-alert-circle' : '$success'" class="custom-alert"
+                v-if="loginResponse" dense text>
                 {{ loginResponse }}
               </v-alert>
               <!-- 登入按鈕，點擊時觸發 login 方法 -->
-              <v-btn block color="purple" dark class="my-2" @click="login"
-                >登入</v-btn
-              >
+              <v-btn block color="purple" dark class="my-2" @click="login">
+                <v-progress-circular v-if="loading" indeterminate color="White"></v-progress-circular>
+                <span v-if="!loading">登入</span>
+              </v-btn>
+
             </v-form>
             <!-- 其他登入選項 -->
             <div class="login-options">
               <!-- Google 登入按鈕 -->
-              <div id="g_id_onload"></div>
+            <!-- Google 登入按鈕 -->
+<div id="g_id_onload" class="my-2 google-btn"></div>
+
 
               <!-- Facebook 登入按鈕 -->
-              <v-btn
-                block
-                color="white"
-                dark
-                class="my-2 d-flex justify-center"
-                @click="loginWithFacebook"
-              >
+              <v-btn block color="white" dark class="my-2 d-flex justify-center" @click="loginWithFacebook">
                 <v-icon left>mdi-facebook</v-icon>
                 以Facebook帳號登入
               </v-btn>
 
               <!-- Twitter 登入按鈕 -->
-              <v-btn
-                block
-                color="white"
-                dark
-                class="my-2 d-flex justify-center"
-                @click="loginWithTwitter"
-              >
+              <v-btn block color="white" dark class="my-2 d-flex justify-center" @click="loginWithTwitter">
                 <v-icon left>mdi-twitter</v-icon>
                 以Twitter帳號登入
               </v-btn>
             </div>
           </v-col>
           <!-- Grid系統的列元件，呈現圖像部分 -->
-          <!-- <v-col cols="6" class="login-image align-self-start custom-class">
-            <spline-component /> -->
-            <!-- SplineComponent元件，用於顯示3D圖像 -->
-          <!-- </v-col> -->
+           <v-col cols="6" class="login-image align-self-start custom-class">
+            <spline-component />
+          <!-- SplineComponent元件，用於顯示3D圖像 -->
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
@@ -111,64 +86,82 @@ export default {
       email: "",
       password: "",
       loginResponse: "",
+      loginAlertType: "error", 
+      loading: false,
     };
   },
   methods: {
     async login() {
       try {
+        this.loading = true
         const response = await axios.post("/users/login", {
           email: this.email,
           password: this.password,
         });
         this.loginResponse = response.data.msg;
+        // 根据响应码改变登入提示類型
+        this.loginAlertType = response.data.code === 20051 ? "success" : "error";
         if (response.data.msg == "登入成功") {
           setTimeout(() => {
+            this.loading = false; // 登入结束，设置 loading 为 false
             this.$router.push("/index");
-          }, 3000);
-        }
+          }, 1000);
+        }else {
+        this.loading = false; // 登入失败，设置 loading 为 false
+      }
         console.log("Logged in: " + response.data.msg + response.data.code);
       } catch (error) {
+        this.loading = false; // 登入失败，设置 loading 为 false
         this.loginResponse = error.message;
       }
     },
 
     //第三方登入google
     async handleCredentialResponse(response) {
-  console.log(response);
-  const id_token = response.credential;
-  let formData = new FormData();   // 建立新的 FormData 實例
-  formData.append('credential', id_token);  // 將資料附加到 formData 上
+      console.log(response);
+      const id_token = response.credential;
+      let formData = new FormData();   // 建立新的 FormData 實例
+      formData.append('credential', id_token);  // 將資料附加到 formData 上
 
-  try {
-    const response = await axios.post("/users/google", formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'  
+      try {
+        const response = await axios.post("/users/google", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        console.log(response);
+        // 根据响应码改变登入提示類型
+        this.loginAlertType = response.data.code === 20051 ? "success" : "error";
+        if (response.data.msg === "登入成功") {
+          // 登入成功後，跳轉到 /index 頁面
+          this.$router.push("/index");
+
+        }
+      } catch (error) {
+        console.error("Error:", error);
       }
-    });
-    console.log(response);
-    if (response.data.msg === "登入成功") {
-      // 登入成功後，跳轉到 /index 頁面
-      this.$router.push("/index");
     }
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
   },
   mounted() {
-  window.googleSignInPromise.then(() => {
-    google.accounts.id.initialize({
-      client_id:
-        '1042407337082-72sipavf4mejlbvb3r5surhov8el6m60.apps.googleusercontent.com',
-      callback: this.handleCredentialResponse,
+    window.googleSignInPromise.then(() => {
+      google.accounts.id.initialize({
+        client_id:
+          '1042407337082-72sipavf4mejlbvb3r5surhov8el6m60.apps.googleusercontent.com',
+        callback: this.handleCredentialResponse,
+      });
+      google.accounts.id.renderButton(
+        document.getElementById("g_id_onload"),
+        {
+          theme: "outline",
+          size: "large"
+        }
+      );
+      // 添加这一行以使图标居中
+      document.getElementById("g_id_onload").firstChild.style.display = 'flex';
+      google.accounts.id.prompt();
     });
-    google.accounts.id.renderButton(
-      document.getElementById("g_id_onload"), 
-      { theme: "outline", size: "large" } 
-    );
-    google.accounts.id.prompt(); 
-  });
 }
+
 };
 </script>
 
@@ -179,6 +172,7 @@ body,
 html {
   font-family: "Roboto", sans-serif;
 }
+
 /* 登入頁面的全局樣式 */
 .login-container {
   height: 100vh;
@@ -197,7 +191,8 @@ html {
 /* SplineComponent 組件中的圖像的樣式 */
 .spline-component img {
   height: 100%;
-  object-fit: cover; /* 讓圖像保持原始比例並覆蓋整個容器 */
+  object-fit: cover;
+  /* 讓圖像保持原始比例並覆蓋整個容器 */
 }
 
 .custom-class {
@@ -214,4 +209,18 @@ html {
 .v-btn {
   text-transform: none;
 }
+
+/* Google 登入按鈕的樣式 */
+.google-btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%; /* 设置按钮宽度与 Facebook 按钮相同 */
+}
+
+/* Google 按钮的图标样式 */
+.google-btn img {
+  margin-right: 8px;
+}
+
 </style>
