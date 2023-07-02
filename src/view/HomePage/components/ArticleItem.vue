49c092b5543e1912b2b4944dd4b77558958e5035
yang <yang87924@gmail.com>
2023-07-02 21:56:57 +0800

    feat: load byscrollTop

--- src/view/HomePage/components/ArticleItem.vue
+++ src/view/HomePage/components/ArticleItem.vue
@@ -27,54 +27,65 @@
                 </div>
             </div>
         </div>
+
     </div>
-</template>
+    <div v-if="isLoading" class="loading">
+        <div class="loader"></div>
+    </div></template>
 <script>
 import axios from 'axios';
-
 export default {
     data() {
         return {
             items: [], // 存放文章的列表
             newArticleTitle: '', // 新文章的標題
             newArticleContent: '', // 新文章的內容
+            pageNum: 1, // 目前頁數
+            isLoading: true, // 是否正在載入中
         };
     },
     mounted() {
         // 在組件載入後，執行非同步行為獲取資料並匯入到items陣列中
         this.fetchData();
+        this.addScrollListener();
     },
     methods: {
         fetchData() {
             // 發送 HTTP GET 請求到後端 API 獲取資料
-            axios.get('/threads/pageThread')
+            axios
+                .get('/threads/pageThread', {
+                    params: {
+                        pageNum: this.pageNum,
+                    },
+                })
                 .then(response => {
                     // 請求成功，將資料設置給items陣列
-                    this.items = response.data.data;
-                    console.log(response.data.data)
+                    this.items = this.items.concat(response.data.data);
+                    console.log(response.data.data);
+                    this.isLoading = false; // 停止載入狀態
                 })
                 .catch(error => {
                     // 請求失敗，處理錯誤
                     console.error(error);
+                    this.isLoading = false; // 停止載入狀態
                 });
-
         },
         addData() {
-            // 發送 HTTP GET 請求到後端 API 獲取資料
-            axios.get('/threads/pageThread')
-                .then(response => {
-                    // 請求成功，將資料設置給items陣列
-                    this.items.concat(response.data.data);
-                    console.log(this.item)
-                })
-                .catch(error => {
-                    // 請求失敗，處理錯誤
-                    console.error(error);
-                });
-
-        }
-    }
-}
+            this.pageNum++; // 增加頁數
+            this.isLoading = true; // 開始載入狀態
+            this.fetchData();
+        },
+        addScrollListener() {
+            window.addEventListener('scroll', () => {
+                const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+                const offset = 5; // 設置一個偏移值
+                if (scrollTop + clientHeight + offset >= scrollHeight) {
+                    this.addData();
+                }
+            });
+        },
+    },
+};
 </script>
 <style lang="css" scoped>
 .down {
@@ -94,7 +105,25 @@ export default {
 
 
 }
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+.loader {
+    border: 16px solid #f3f3f3; /* Light grey */
+    border-top: 16px solid #3498db; /* Blue */
+    border-radius: 50%;
+    width: 120px;
+    height: 120px;
+    animation: spin 2s linear infinite;
+}
 
+.loading {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh; /* or any other value that suits your needs */
+}
 .avatar {
     height: 50px;
     width: 50px;
