d95e40d3b804987c41d8589d5d39f522d4a1e57c
cherry <shps950421@gmail.com>
2023-07-02 20:35:35 +0800

    dataimport

--- src/view/HomePage/components/ArticleItem.vue
+++ src/view/HomePage/components/ArticleItem.vue
@@ -1,7 +1,7 @@
 <template >
-    <div v-for="(item, index) in items" :key="index" class="articleitem">
-        <div class="img">
-            <img :src="item.picture" alt="">
+    <div  v-for="(item, index) in items" :key="index" class="articleitem" >
+        <div class="newsimg">
+            <img :src="item.picture" alt="" class="newspic">
         </div>
         <div class="data">
             <div class="top">
@@ -9,14 +9,14 @@
                 <div class="icon"><img src="../../../assets/img/HomePage/ArticleItem/Love.svg" alt=""></div>
             </div>
             <div class="tags">
-                <span v-for="tag in item.tags" :key="tag">{{ tag }}</span>
-                
+                <span v-for="tag in item.hashtags" :key="tag">{{ tag.name }}</span>
+
             </div>
             <div class="down">
                 <div class="person">
-                    <img :src="item.picture" alt="">
+                    <img :src="item.user.avatar" alt="" class="avatar">
                     <div class="name-area">
-                        <div class="name">{{ item.author }}</div>
+                        <div class="name">{{ item.user.userName }}</div>
                         <div class="time">{{ item.createdAt }}</div>
                     </div>
                 </div>
@@ -28,8 +28,6 @@
             </div>
         </div>
     </div>
-    
-    
 </template>
 <script>
 import axios from 'axios';
@@ -49,13 +47,25 @@ export default {
     methods: {
         fetchData() {
             // 發送 HTTP GET 請求到後端 API 獲取資料
-            axios.get('/threads')
+            axios.get('/threads/pageThread')
                 .then(response => {
                     // 請求成功，將資料設置給items陣列
                     this.items = response.data.data;
-                    
-
                     console.log(response.data.data)
+                })
+                .catch(error => {
+                    // 請求失敗，處理錯誤
+                    console.error(error);
+                });
+
+        },
+        addData() {
+            // 發送 HTTP GET 請求到後端 API 獲取資料
+            axios.get('/threads/pageThread')
+                .then(response => {
+                    // 請求成功，將資料設置給items陣列
+                    this.items.concat(response.data.data);
+                    console.log(this.item)
                 })
                 .catch(error => {
                     // 請求失敗，處理錯誤
@@ -68,26 +78,38 @@ export default {
 </script>
 <style lang="css" scoped>
 .down {
-    margin-top: 40px;
+    margin-top: 70px;
     display: flex;
     align-items: center;
     justify-content: space-between;
+    
 
 }
 
 .person {
+    height: 50px;
+    width: 200px;
     display: flex;
     align-items: center;
+
+
+}
+
+.avatar {
+    height: 50px;
+    width: 50px;
+    clip-path: circle(50% at center);
 
 }
 
 .articleitem {
     width: 100%;
     display: flex;
-    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    box-shadow: 4px 4px rgba(0, 0, 0, 0.25);
     padding-left: 21.48459243774414px;
-    padding-top: 21.48459243774414px;
+    /* padding-top: 21.48459243774414px; */
     border-radius: 17.187673568725586px;
+    margin-bottom: 20px;
 }
 
 .top {
@@ -99,7 +121,8 @@ export default {
 }
 
 .data {
-    width: 100%;
+    width: 60%;
+    margin-left: 10px;
 
 }
 
@@ -160,5 +183,25 @@ export default {
     text-align: left;
     color: #858EAD;
     margin-right: 10px;
+}
+
+.newsimg {
+
+    height: 150px;
+    width: 150px;
+
+    box-shadow: 5px 5px rgba(0, 0, 0, 0.25);
+    border-radius: 10%;
+    margin: 10px 0px 10px 0px;
+    
+
+}
+
+.newspic {
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+    border-radius: 10%;
+
 }
 </style>
