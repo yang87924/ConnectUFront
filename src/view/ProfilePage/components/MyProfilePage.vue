69fd1115cd53f01da1f779ebcdede5a2010949fc
chainheaven <chainheaven@gmail.com>
2023-07-07 13:59:09 +0800

    123

--- src/view/ProfilePage/components/MyProfilePage.vue
+++ src/view/ProfilePage/components/MyProfilePage.vue
@@ -61,7 +61,7 @@ export default {
     fetchData() {
       // 發送 HTTP GET 請求到後端 API 獲取資料
       axios
-        .get("/dyThreads/pageDyThread", {
+        .get("dyThreads/userDyThread/2", {
           params: {
             pageNum: this.pageNum,
           },
