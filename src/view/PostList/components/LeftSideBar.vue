8bbde663e51131e5fb35e10c19c1f147552a1d80
Snoop33 <gpx5992189@gmail.com>
2023-07-07 15:17:12 +0800

    leftSiDEbAR

--- src/view/PostList/components/LeftSideBar.vue
+++ src/view/PostList/components/LeftSideBar.vue
@@ -1,108 +1,78 @@
-<template lang="">
-    <div class="content flex-between" v-for="(item, index) in items" :key="index">
-        <div class="trending">
-            <!-- <div class="subtitle">
-                Trending in Turkey
-            </div> -->
-            <div class="tag">
-                {{ item.name }}
-            </div>
-            <div class="count">
-                {{ item.amount }} Tweets
-            </div>
+<template>
+    <div class="search-container">
+      <input
+        type="text"
+        v-model="query"
+        @input="onInput"
+        placeholder="Search..."
+        class="search-input"
+      />
+      <div v-if="results.length" class="results-container animate__animated animate__fadeIn">
+        <div
+          v-for="result in results"
+          :key="result"
+          class="result-item"
+        >
+          {{ result }}
         </div>
-        <div class="tool">
-            <span class="material-icons">keyboard_arrow_down</span>
-        </div>
+      </div>
     </div>
-</template>
-<script>
-import axios from "axios";
-
-export default {
-  data() {
-    return {
-      items: [], // 存放動態的列表
-    };
-  },
-  mounted() {
-    // 在組件載入後，執行非同步行為獲取資料並匯入到items陣列中
-    // this.fetchData();
-  },
-  created() {
-    // 查詢流行趨勢的API
-        axios.get('/dyThreads/dyHotHashtag')
-            .then(response => {
-                console.log('流行趨勢：', response.data.data);
-                // console.log(response.data);
-                if (response.data.data && response.data.data.length > 0) {
-                    this.items = this.items.concat(response.data.data);
-                } else {
-                    console.log('回應資料無效或沒有文章資料');
-                    this.item = '找不到流行趨勢';
-                }
-            })
-            .catch(error => {
-                console.log('發生錯誤：', error);
-                this.item = '找不到流行趨勢';
-            });
-  },
-}
-</script>
-<style lang="css" scoped>
-.content {
-    border-top: 1px solid lightgrey;
-    border-bottom: 1px solid lightgrey;
-    padding: 12px 24px;
+  </template>
+  
+  <script>
+  import { ref, watch } from 'vue';
+  
+  export default {
+    setup() {
+      const query = ref('');
+      const results = ref([]);
+  
+      const onInput = () => {
+        // 模拟搜索结果
+        results.value = query.value ? [query.value, query.value + ' result'] : [];
+      };
+  
+      return { query, results, onInput };
+    },
+  };
+  </script>
+  
+<style scoped>
+.search-container {
+  position: relative;
+  width: 300px; /* 调整宽度 */
+  margin: 50px auto; /* 调整上下位置 */
 }
 
-.trending div:not(:last-child) {
-    margin-bottom: 4px;
+.search-input {
+  width: 100%;
+  padding: 12px 40px 12px 12px; /* 调整内边距 */
+  font-size: 16px;
+  border-radius: 4px;
+  border: 1px solid #ccc;
+  background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.064.072.098.112l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.096zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>');
+  background-position: right 12px center; /* 放大镜位置 */
+  background-repeat: no-repeat;
 }
 
-.flex-center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
+.results-container {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  width: 100%;
+  border: 1px solid #ccc;
+  border-top: none;
+  border-radius: 0 0 4px 4px;
+  max-height: 150px;
+  overflow-y: auto;
 }
 
-.flex-between {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+.result-item {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
 }
 
-.subtitle {
-    font-family: 'SF Pro Text';
-    font-size: 13px;
-    font-weight: 400;
-    line-height: 16px;
-    letter-spacing: 0em;
-    text-align: left;
-    color: #6E767D;
-}
-
-.tag {
-    font-family: 'SF Pro Text';
-    font-size: 15px;
-    font-weight: 700;
-    line-height: 18px;
-    letter-spacing: 0em;
-    text-align: left;
-    color: var(--black);
-}
-
-.count {
-    font-family: 'SF Pro Text';
-    font-size: 15px;
-    font-weight: 400;
-    line-height: 18px;
-    letter-spacing: 0em;
-    text-align: left;
-    color: #6E767D;
-}
-
-.material-icons {
-    cursor: pointer;
+.result-item:last-child {
+  border-bottom: none;
 }
 </style>
