35b469d122d1dcc81c3d89d17a7334def5c111eb
SeeIetw <star159357tw@gmail.com>
2023-07-07 09:01:14 +0800

    fix chat

---
+++ src/view/Chatroom/Chatroom.vue
@@ -0,0 +1,182 @@
+<template>
+    <div>
+        <div class="abs cover contaniner">
+            <div class="abs cover pnl">
+                <div class="top pnl-head" style="padding: 20px ; color: white;">
+                    <div id="userName"> 用户：张三<span style='float: right;color: green'>在线</span></div>
+                    <div id="chatMes" style="text-align: center;color: #6fbdf3;font-family: 新宋体">
+                        <!--正在和 <font face="楷体">张三</font> 聊天-->
+                    </div>
+                </div>
+                <!--聊天区开始-->
+                <div class="abs cover pnl-body" id="pnlBody">
+                    <div class="abs cover pnl-left" id="initBackground" style="background-color: white; width: 100%">
+                        <div class="abs cover pnl-left" id="chatArea" style="display: none">
+                            <div class="abs cover pnl-msgs scroll" id="show">
+                                <div class="pnl-list" id="hists"><!-- 历史消息 --></div>
+                                <div class="pnl-list" id="msgs">
+
+                                    <!-- 消息这展示区域 -->
+                                    <!-- <div class="msg guest"><div class="msg-right"><div class="msg-host headDefault"></div><div class="msg-ball">你好</div></div></div>
+                                     <div class="msg robot"><div class="msg-left" worker=""><div class="msg-host photo" style="background-image: url(img/avatar/Member002.jpg)"></div><div class="msg-ball">你好</div></div></div>-->
+                                </div>
+                            </div>
+
+                            <div class="abs bottom pnl-text">
+                                <div class="abs cover pnl-input">
+                            <textarea class="scroll" id="context_text" wrap="hard"
+                                      placeholder="在此输入文字信息..."></textarea>
+                                    <div class="abs atcom-pnl scroll hide" id="atcomPnl">
+                                        <ul class="atcom" id="atcom"></ul>
+                                    </div>
+                                </div>
+
+                                <div class="abs br pnl-btn" id="submit"
+                                     style="background-color: rgb(32, 196, 202); color: rgb(255, 255, 255);">
+                                    发送
+                                </div>
+                            </div>
+                        </div>
+
+                        <!--聊天区 结束-->
+
+                        <div class="abs right pnl-right">
+                            <div class="slider-container hide"></div>
+                            <div class="pnl-right-content">
+                                <div class="pnl-tabs">
+                                    <div class="tab-btn active" id="hot-tab">好友列表</div>
+                                </div>
+                                <div class="pnl-hot">
+                                    <ul class="rel-list unselect" id="userlist">
+                                        <li class="rel-item"><a onclick='showChat("张三")'>张三</a></li>
+                                        <li class="rel-item"><a onclick='showChat("李四")'>李四</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="pnl-right-content">
+                                <div class="pnl-tabs">
+                                    <div class="tab-btn active">系统广播</div>
+                                </div>
+                                <div class="pnl-hot">
+                                    <ul class="rel-list unselect" id="broadcastList">
+                                        <!--<li class="rel-item" style="color: #9d9d9d;font-family: 宋体">您的好友 张三 已上线</li>
+                                        <li class="rel-item" style="color: #9d9d9d;font-family: 宋体">您的好友 李四 已上线</li>-->
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import $ from 'jquery'
+import axios from "axios";
+export default {
+    name: 'Chatroom',
+    data() {
+        return {
+            toName: "",
+            username: ""
+        };
+    },
+    methods: {
+        showChat(name) {
+            this.toName = name;
+            $("#chatArea").css("display", "inline");
+            $("#msgs").html("");
+            $("#chatMes").html("正在和 <font face=\"楷体\">" + this.toName + "</font> 聊天");
+            const chatData = sessionStorage.getItem(this.toName);
+            if (chatData != null) {
+                $("#msgs").html(chatData);
+            }
+        }
+
+
+    },
+    mounted() {
+        const userName = this.userName;
+        const toName = this.toName;
+        $(function () {
+            axios.get('users/getUserName')
+                .then(res => {
+                    console.log("success......."+res.data);
+                    this.userName = res.data;
+                    $("#userName").html(" 用户：" + res + "<span style='float: right;color: green'>在線</span>");
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+
+      const ws = new WebSocket("ws://localhost:8080/chat");
+
+      ws.onopen = function () {
+        console.log("on open...");
+        $("#userName").html(" 用户：" + userName + "<span style='float: right;color: green'>在线</span>");
+      };
+
+      ws.onmessage = function (evt) {
+
+        console.log("on message...");
+        const dataStr = evt.data;
+        const res = JSON.parse(dataStr);
+        console.log(res);
+        if (res.system) {
+          const names = res.message;
+          let userlistStr = "";
+          let broadcastListStr = "";
+          for (const name of names) {
+            if (name !== userName) {
+                console.log(name);
+                console.log(userName);
+              userlistStr += "<li class=\"rel-item\"><a onclick='showChat(\"" + name + "\")'>" + name + "</a></li>";
+              broadcastListStr += "<li class=\"rel-item\" style=\"color: #9d9d9d;font-family: 宋体\">您的好友 " + name + " 已上线</li>";
+            }
+          }
+          console.log(userlistStr);
+          $("#userlist").html(userlistStr);
+          $("#broadcastList").html(broadcastListStr);
+        } else {
+          let str = "<div class=\"msg robot\"><div class=\"msg-left\" worker=\"\"><div class=\"msg-host photo\" style=\"background-image: url(https://seeiebucket.s3.ap-northeast-1.amazonaws.com/d515c3c1-73bb-497f-b529-f10e8da4df49.png)\"></div><div class=\"msg-ball\">" + res.message + "</div></div></div>";
+          if (toName === res.fromName) {
+            $("#msgs").append(str);
+          }
+          const chatData = sessionStorage.getItem(res.fromName);
+          if (chatData != null) {
+            str = chatData + str;
+          }
+          sessionStorage.setItem(res.fromName, str);
+        }
+      };
+
+      ws.onclose = function () {
+        console.log("on close...");
+        $("#userName").html(" 用户：" + userName + "<span style='float: right;color: red'>离线</span>");
+      };
+
+      $("#submit").click(function () {
+        const data = $("#context_text").val();
+        $("#context_text").val("");
+        const json = {"toName": toName, "message": data};
+        let str = "<div class=\"msg guest\"><div class=\"msg-right\"><div class=\"msg-host headDefault\"></div><div class=\"msg-ball\">" + data + "</div></div></div>";
+        $("#msgs").append(str);
+        const chatData = sessionStorage.getItem(toName);
+        if (chatData != null) {
+          str = chatData + str;
+        }
+        sessionStorage.setItem(toName, str);
+        ws.send(JSON.stringify(json));
+      });
+    });
+  }
+};
+</script>
+
+<style scoped lang="css">
+@import "css/bootstrap.min.css";
+@import "css/chat.css";
+</style>
