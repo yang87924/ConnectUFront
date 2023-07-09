4415143fc2e9d7fce69b4059e5aea806d5b6c5eb
Snoop33 <gpx5992189@gmail.com>
2023-07-09 17:29:37 +0800

    rightsidebar

--- src/view/PostList/components/RightSideBar.vue
+++ src/view/PostList/components/RightSideBar.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="search-container">
+  <div class="search-container">
+    <div class="search-wrapper">
       <input
         type="text"
         v-model="query"
@@ -7,52 +8,133 @@
         placeholder="Search..."
         class="search-input"
       />
-      <div v-if="results.length" class="results-container animate__animated animate__fadeIn">
-        <div
-          v-for="result in results"
-          :key="result"
-          class="result-item"
-        >
-          {{ result }}
-        </div>
+      <div class="search-icon"></div>
+    </div>
+    <div class="hashtags-container">
+      <div
+        v-for="tag in hashtags"
+        :key="tag"
+        class="hashtag"
+        @click="addTagToQuery(tag)"
+      >
+        {{ tag }}
       </div>
     </div>
-  </template>
-  
-  <script>
-  import { ref, watch } from 'vue';
-  
-  export default {
-    setup() {
-      const query = ref('');
-      const results = ref([]);
-  
-      const onInput = () => {
-        // 模拟搜索结果
-        results.value = query.value ? [query.value, query.value + ' result'] : [];
-      };
-  
-      return { query, results, onInput };
-    },
-  };
-  </script>
-  
+    <div v-if="results.length" class="results-container animate__animated animate__fadeIn">
+      <div
+        v-for="result in results"
+        :key="result"
+        class="result-item"
+      >
+        {{ result }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+import axios from 'axios';
+
+export default {
+  emits: ['update-items'],
+  setup(_, { emit }) {
+    const query = ref('');
+    const results = ref([]);
+    const hashtags = ref(['求職', '運動', '美食','游戲','音樂']); // 标签列表
+
+    const onInput = async () => {
+      if (query.value) {
+        try {
+          const response = await axios.get('/dyThreads/search', {
+            params: {
+              query: query.value,
+            },
+          });
+          results.value = response.data;
+
+          emit('update-items', response.data);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        results.value = [];
+        emit('update-items', []);
+      }
+    };
+
+    const addTagToQuery = (tag) => {
+      query.value += `#${tag} `;
+    };
+
+    return { query, results, hashtags, onInput, addTagToQuery };
+  },
+};
+</script>
+
 <style scoped>
 .search-container {
   position: relative;
-  width: 300px; /* 调整宽度 */
-  margin: 50px auto; /* 调整上下位置 */
+  width: 300px;
+  margin: 50px auto;
+}
+
+.search-wrapper {
+  position: relative;
 }
 
 .search-input {
   width: 100%;
-  padding: 12px 40px 12px 12px; /* 调整内边距 */
+  padding: 12px 40px 12px 12px;
   font-size: 16px;
   border-radius: 4px;
-  border: 1px solid #ccc;
+  border: none;
+  background-color: #f1f1f1;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  transition: background-color 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+}
+
+.search-input:focus {
+  outline: none;
+  background-color: #ffffff;
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
+}
+
+.search-icon {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  width: 20px;
+  height: 20px;
   background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.064.072.098.112l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.096zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg>');
-  background-position: right 12px center; /* 放大镜位置 */
+  background-position: center;
   background-repeat: no-repeat;
+  background-color: transparent;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.search-icon:hover {
+  background-color: #f1f1f1;
+}
+
+.hashtags-container {
+  margin-top: 10px;
+}
+
+.hashtag {
+  display: inline-block;
+  margin-right: 5px;
+  padding: 5px 10px;
+  background-color: #eee;
+  border-radius: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.hashtag:hover {
+  background-color: #ddd;
 }
 
 .results-container {
@@ -65,11 +147,17 @@
   border-radius: 0 0 4px 4px;
   max-height: 150px;
   overflow-y: auto;
+  transition: all 0.3s ease-in-out;
 }
 
 .result-item {
   padding: 10px;
   border-bottom: 1px solid #eee;
+  transition: all 0.3s ease-in-out;
+}
+
+.result-item:hover {
+  background-color: #f5f5f5;
 }
 
 .result-item:last-child {
