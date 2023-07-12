484c0bfada5b3f5f6657cc7339dd052a9f071f57
chainheaven <chainheaven@gmail.com>
2023-07-12 23:44:51 +0800

    fix profile

--- src/view/ProfilePage/components/MyProfilePage.vue
+++ src/view/ProfilePage/components/MyProfilePage.vue
@@ -59,96 +59,118 @@ import axios from "axios";
 import { reactive } from "vue";//在 Vue 3 中引入 reactive 函數的語法
 
 export default {
-  data() {
-    return {
-      items: [], // 存放動態的列表
-      pageNum: 1, // 目前頁數
-      isLoading: true, // 是否正在載入中
-      loveStatus: false, //使用者是否有按讚
-    };
-  },
-  mounted() {
-    // 在組件載入後，執行非同步行為獲取資料並匯入到items陣列中
-    this.fetchData();
-    this.addScrollListener();
-  },
-  //-----
-  methods: {
-    fetchData() {
-      // 發送 HTTP GET 請求到後端 API 獲取資料
-      axios
-        .get("/dyThreads/userDyThread/0", {
-          params: {
-            pageNum: this.pageNum,
-          },
-        })
-        .then((response) => {
-          // 請求成功，將資料設置給items陣列
-          this.items = this.items.concat(response.data.data);
-          console.log(response.data.data);
-          this.isLoading = false; // 停止載入狀態
-        })
-        .catch((error) => {
-          // 請求失敗，處理錯誤
-          console.error(error);
-          this.isLoading = false; // 停止載入狀態
-        });
+props: ['results'],
+data() {
+  return {
+    items: [], // 存放動態的列表
+    pageNum: 1, // 目前頁數
+    isLoading: true, // 是否正在載入中
+    loveStatus: false, //使用者是否有按讚
+    showDialog: false,
+    selectedImage: ""
+  };
+},
+watch: {
+  results: {  // 当父组件传递的搜索结果更新时，使用watch来监听
+    handler(newVal, oldVal) {
+      if(newVal.length > 0) {
+        this.items = newVal;  // 更新items
+      }
     },
-    addData() {
-      this.pageNum++; // 增加頁數
-      this.isLoading = true; // 開始載入狀態
-      this.fetchData();
-    },
-    addScrollListener() {
-      window.addEventListener("scroll", () => {
-        const { scrollTop, clientHeight, scrollHeight } =
-          document.documentElement;
-        const offset = 5; // 設置一個偏移值
-        if (scrollTop + clientHeight + offset >= scrollHeight) {
-          this.addData();
-        }
+    deep: true,
+  },
+},
+mounted() {
+  // 在組件載入後，執行非同步行為獲取資料並匯入到items陣列中
+  this.fetchData();
+  this.addScrollListener();
+},
+//-----
+methods: {
+  fetchData() {
+    // 發送 HTTP GET 請求到後端 API 獲取資料
+    axios
+      .get("/dyThreads/userDyThread/0", {
+        params: {
+          pageNum: this.pageNum,
+        },
+      })
+      .then((response) => {
+        // 請求成功，將資料設置給items陣列
+        this.items = this.items.concat(response.data.data);
+        console.log(response.data.data);
+        this.isLoading = false; // 停止載入狀態
+      })
+      .catch((error) => {
+        // 請求失敗，處理錯誤
+        console.error(error);
+        this.isLoading = false; // 停止載入狀態
       });
-    },
-
-    toggleLove(item) {
-      const loveId = item.dyThreadId;
-      axios
-        .put("dyThreads/toggleUserLove/" + loveId)
-        .then((response) => {
-          console.log(response.data);
-          if (item.loveStatus === 1) {
-            item.loveStatus = 0;
-            item.love--;
-          } else {
-            item.loveStatus = 1;
-            item.love++;
-          }
-          item = reactive(item);//實現數據的響應式
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    },
   },
+  addData() {
+    this.pageNum++; // 增加頁數
+    this.isLoading = true; // 開始載入狀態
+    this.fetchData();
+  },
+  addScrollListener() {
+    let isBottomReached = false; // 標誌是否滾動到底部
+
+    window.addEventListener("scroll", () => {
+      const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
+      const offset = 5; // 設置一個偏移值
+
+      if (scrollTop + clientHeight + offset >= scrollHeight
+      //  && !isBottomReached
+      ) {
+        // isBottomReached = true; // 設置標誌為 true，避免再次觸發
+        this.addData();
+      }
+    });
+  },
+
+  toggleLove(item) {
+    const loveId = item.dyThreadId;
+    axios
+      .put("dyThreads/toggleUserLove/" + loveId)
+      .then((response) => {
+        console.log(response.data);
+        if (item.loveStatus === 1) {
+          item.loveStatus = 0;
+          item.love--;
+        } else {
+          item.loveStatus = 1;
+          item.love++;
+        }
+        item = reactive(item);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  showImageDialog(image) {
+    this.selectedImage = image;
+    this.showDialog = true;
+  }
+},
 };
 </script>
 <style lang="css" scoped>
 .tweet {
-  border-top: 1px solid #ebeef0;
-  width: auto;
-  background-color: #fff;
-  padding: 8px 16px;
+border-top: 1px solid #ebeef0;
+width: auto;
+background-color: #fff;
+padding: 8px 16px;
 
-  font-family: "微軟正黑體";
-  font-size: 19px;
-  font-weight: 400;
-  line-height: 22px;
-  letter-spacing: 0em;
-  text-align: left;
+font-family: "微軟正黑體";
+font-size: 19px;
+font-weight: 400;
+line-height: 22px;
+letter-spacing: 0em;
+text-align: left;
 
-  cursor: pointer;
+cursor: pointer;
 
-  display: flex;
+display: flex;
 }
 
 .place {
@@ -157,136 +179,136 @@ font-family: "ABeeZee";
 }
 
 .flex-center {
-  display: flex;
-  justify-content: center;
-  align-items: center;
+display: flex;
+justify-content: center;
+align-items: center;
 }
 
 .flex-between {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+display: flex;
+justify-content: space-between;
+align-items: center;
 }
 
 .flex-start {
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  /* margin-left: 200px; */
+display: flex;
+justify-content: flex-start;
+align-items: center;
+/* margin-left: 200px; */
 }
 
 .friend-box {
-  height: 100%;
-  /* background: #1e90ff; */
+height: 100%;
+/* background: #1e90ff; */
 }
 
 .friendimg-box {
-  width: 60.55px;
-  height: 60.55px;
-  margin-right: 12px;
-  margin-bottom: auto;
+width: 60.55px;
+height: 60.55px;
+margin-right: 12px;
+margin-bottom: auto;
 }
 
 .friendimg-box img {
-  border-radius: 50%;
-  width: 55px;
-  height: 55px;
-  object-fit: cover;
+border-radius: 50%;
+width: 55px;
+height: 55px;
+object-fit: cover;
 }
 
 .content {
-  width: 100%;
+width: 100%;
 }
 
 .content div {
-  margin-bottom: 12px;
+margin-bottom: 12px;
 }
 
 .tags {
-    width: 100%;
+  width: 100%;
 }
 
 .tags span {
-    width: 53.48px;
-    height: 24.59px;
-    padding: 4.2969183921813965px 10.74229621887207px 4.2969183921813965px 10.74229621887207px;
-    border-radius: 21.48459243774414px;
-    background-color: #F4F6F8;
-    font-family: Source Sans Pro;
-    font-size: 15px;
-    font-weight: 600;
-    line-height: 15px;
-    letter-spacing: 0em;
-    text-align: left;
-    color: #858EAD;
-    margin-right: 10px;
-    cursor: pointer;
+  width: 53.48px;
+  height: 24.59px;
+  padding: 4.2969183921813965px 10.74229621887207px 4.2969183921813965px 10.74229621887207px;
+  border-radius: 21.48459243774414px;
+  background-color: #F4F6F8;
+  font-family: Source Sans Pro;
+  font-size: 15px;
+  font-weight: 600;
+  line-height: 15px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #858EAD;
+  margin-right: 10px;
+  cursor: pointer;
 }
 
 .friend-info span {
-  margin-right: 8px;
+margin-right: 8px;
 }
 
 .place {
-  color: #5b7083;
+color: #5b7083;
 }
 
 /* .img-box {
-  border-radius: 20px;
-  width: 628.94px;
-  height: 305.2px;
-  background-repeat: no-repeat;
-  background-position: center;
-  background-size: contain;
+border-radius: 20px;
+width: 628.94px;
+height: 305.2px;
+background-repeat: no-repeat;
+background-position: center;
+background-size: contain;
 } */
 .img-container {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr); /* 在这里设置你想要的列数 */
-  grid-gap: 10px; 
-  width: 628.94px;
+display: grid;
+grid-template-columns: repeat(2, 1fr); /* 在这里设置你想要的列数 */
+grid-gap: 10px; 
+width: 628.94px;
 }
 
 .img-box {
-  width: 100%;
-  position: relative;
-  overflow: hidden;
-  aspect-ratio: 2/1;
-  border: 0.8px solid #5b7083;
-  border-radius: 8%;
+width: 100%;
+position: relative;
+overflow: hidden;
+aspect-ratio: 2/1;
+border: 0.8px solid #5b7083;
+border-radius: 8%;
 }
 
 .img-box img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+width: 100%;
+height: 100%;
+object-fit: cover;
 }
 
 .tweetImg {
-  width: 100%;
+width: 100%;
 }
 
 .fn {
-  color: #5b7083;
-  margin-right: 70px;
-  margin-left: 150px;
+color: #5b7083;
+margin-right: 70px;
+margin-left: 150px;
 }
 
 .fn span:not(:last-child) {
-  margin-right: 8px;
+margin-right: 8px;
 }
 
 /* .active {
-    color: #F4245E;
+  color: #F4245E;
 } */
 
 .show {
-  color: #1da1f2;
-  font-size: 16.063px;
-  font-family: "ABeeZee";
-  /* font-style: italic; */
+color: #1da1f2;
+font-size: 16.063px;
+font-family: "ABeeZee";
+/* font-style: italic; */
 }
 
 .active {
-  color: #1e90ff;
+color: #1e90ff;
 }
 </style>
